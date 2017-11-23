8fa8c744935144a340b10c09606722f3273d7467
Adel Beitvashahi <Adelbeit@indiana.edu>
2017-11-23 01:47:48 -0500

    first heuristic, check number of tiles per state

---
+++ heuristic1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#import search
+import numpy as np
+from copy import deepcopy as dc
+def heuristic1(board):
+    score = 0
+    mx = 0
+    numtiles = 0
+    for row in board:
+        for tile in row:
+            if tile > mx:
+                mx = tile
+            if tile != 0:
+                numtiles+=1
+                score += math.log(tile,2)
+    return (score,mx,numtiles)
